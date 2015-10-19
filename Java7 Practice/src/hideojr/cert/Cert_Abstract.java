8dda3cb4359f05cd649357f1913ca5cff251cd40
hideojr <hideojr@gmail.com>
2015-10-19 12:45:05 +0800

    test git function

--- Java7 Practice/src/hideojr/cert/Cert_Abstract.java
+++ Java7 Practice/src/hideojr/cert/Cert_Abstract.java
@@ -1,5 +1,5 @@
 package hideojr.cert;
 
-public abstract class Cert_Abstract { //這樣宣告也行abstract public class Cert_Abstract
-
+public abstract class Cert_Abstract { //這樣宣告也行abstract public class Cert_Abstract 
+	public abstract void dSomething();
 }
