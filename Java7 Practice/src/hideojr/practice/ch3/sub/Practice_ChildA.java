a102a050e2d0b323ae2f01da4c81c1169cdd7971
Student <Student@EEIT-PC>
2016-02-15 09:50:45 +0800

    modified

--- Java7 Practice/src/hideojr/practice/ch3/sub/Practice_ChildA.java
+++ Java7 Practice/src/hideojr/practice/ch3/sub/Practice_ChildA.java
@@ -1,7 +1,5 @@
 package hideojr.practice.ch3.sub;
 
-import java.io.IOException;
-
 import hideojr.practice.ch3.Practice_Father;
 
 /**
@@ -16,7 +14,16 @@ public class Practice_ChildA extends Practice_Father {
 	//Compile warning:The method Practice_ChildA.showInfo() does not override the inherited method from Practice_Father since it is private to a different package
 	public void showInfo() {	 //該方法與父類別的showInfo方法是沒有覆寫關係的，可用Annotation @Override幫你檢查
 		System.out.println("I am Practice_ChildA!");
+		System.out.println("I am Practice_ChildA " + name1);
 		return; //無回傳值得方法，也可以適時地在程式中加入以結束方法呼叫
+	}
+	
+	public String getMsg(Practice_Father obj) {
+		return null; //obj.name1; //compile error: The field Practice_Father.name1 is not visible
+	}
+	
+	public String getMsg(Practice_ChildA obj) {
+		return obj.name1;
 	}
 
 	protected interface ChildAInnerInterface {
